4b25e71b332e7f9333d7cbc08d3b0c168de9a1fe
zubaidr <web.expert1221@gmail.com>
2022-07-22 23:37:32 +0500

    Worked on the Details page and comment section as well

--- packages/client/src/components/Navbar.tsx
+++ packages/client/src/components/Navbar.tsx
@@ -1,16 +1,25 @@
 import * as React from "react";
+import { useState } from "react";
 import Box from "@mui/material/Box";
 import CssBaseline from "@mui/material/CssBaseline";
 import MuiAppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
+import { NavLink as RouterNavLink } from "react-router-dom";
+import { styled } from "@mui/material/styles";
+import { Comment } from "../types";
+
+const LinkStlyed = styled(RouterNavLink)(({ theme }) => ({
+  textDecoration: "none",
+}));
 
 export function Navbar() {
+
   return (
     <Box sx={{ display: "flex" }}>
       <CssBaseline />
-      <MuiAppBar position="fixed">
+      <MuiAppBar position="fixed" sx={{ background: "#29343B " }}>
         <Toolbar>
           <IconButton
             color="inherit"
@@ -18,9 +27,21 @@ export function Navbar() {
             edge="start"
             sx={{ mr: 2 }}
           ></IconButton>
-          <Typography variant="h6" noWrap component="div">
-            Penze
-          </Typography>
+          <LinkStlyed to="/Details">
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{
+                fontSize: "30px",
+                fontWeight: "700",
+                color: "#fff",
+                ml: "24",
+              }}
+            >
+              Penze
+            </Typography>
+          </LinkStlyed>
         </Toolbar>
       </MuiAppBar>
     </Box>
---
+++ packages/client/src/types/index.ts
@@ -0,0 +1,4 @@
+export interface Comment {
+    name:string;
+    message:string
+}
